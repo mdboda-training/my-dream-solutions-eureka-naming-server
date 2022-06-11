731742d48004b4454b1a4596dc54251c62b85cb2
MuraliDhar Boda <100523854+mdboda-training@users.noreply.github.com>
2022-06-11 14:12:35 +0530

    Create MyDreamSolutionsEurekaNamingServerApplication.java

---
+++ src/main/java/com/mdsolutions/MyDreamSolutionsEurekaNamingServerApplication.java
@@ -0,0 +1,13 @@
+package com.mdsolutions;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyDreamSolutionsEurekaNamingServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyDreamSolutionsEurekaNamingServerApplication.class, args);
+	}
+
+}
